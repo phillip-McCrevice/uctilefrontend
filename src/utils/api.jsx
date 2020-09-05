d56363bb248c7b9d02a34a0f25430ba10e1eb0ad
phillip-McCrevice <tvans12351@gmail.com>
2020-09-04 22:24:38 -0400

    fixed deploy issue

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const api = axios.create({
 	// baseURL: 'http://localhost:1337',
-	baseURL: 'https://uctile-api.herokuapp.com'
+	baseURL: 'https://uctile-api.herokuapp.com',
 	headers: {
 		'Content-Type': 'application/json'
 	}
